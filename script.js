ae205b40f31c27ae96bd720feda191cff6f7f77c
Claire Goeckner-Wald <claire@caltech.edu>
2020-06-27 13:52:23 -0400

    Update colors post-UI change

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* 	Color reference chart:
 	0: #ebedf0
-	1: #c6e48b
-	2: #7bc96f
-	3: #239a3b
-	4: #196127
+	1: #c6e48b -> #9be9a8
+	2: #7bc96f -> #40c463
+	3: #239a3b -> #30a14e
+	4: #196127 -> #216e39
+
+	black: rgb(36, 41, 46);
+	sub-heading grey: rgb(102, 102, 102);
+	paragraph grey: rgb(88, 96, 105);;
 */
 
 color_map = {
 		0: "#ebedf0",
-		1: "#c6e48b",
-		2: "#7bc96f",
-		3: "#239a3b",
-		4: "#196127",
+		1: "#9be9a8",
+		2: "#40c463",
+		3: "#30a14e",
+		4: "#216e39",
 	}
 
 /* Advances stored color for rect x */
