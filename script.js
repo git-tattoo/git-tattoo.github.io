8ac0a741bb5d6bb895993b56bb1010d71e23781d
Claire Goeckner-Wald <claire@caltech.edu>
2020-06-27 16:05:09 -0400

    Add preset functionality

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ color_map = {
 		4: "#216e39",
 	}
 
+preset_map = {
+	"HIRE ME": new Array(371).fill(0),
+	"RANDOM 1": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 2": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 3": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 4": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 5": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 6": new Array(53).fill(new Array(7).fill(0)),
+	"RANDOM 7": new Array(53).fill(new Array(7).fill(0)),
+}
+
 /* Advances stored color for rect x */
 function increment(x) {
 	if (typeof x.getAttribute("data-count") != "string") {
@@ -28,8 +39,45 @@ function increment(x) {
 	x.style.fill = color_map[count];
 }
 
+function apply_preset(key) {
+	let svg = document.getElementById("svg");
+	let i = 0;
+	for (let g of svg.childNodes) {
+		if (g.nodeName != "#text") {
+			for (let r of g.childNodes) {
+				if (r.nodeName != "#text") {
+					r.style.fill = color_map[preset_map[key][i]];
+					i += 1;
+				}
+			}
+		}
+	}
+}
+
+function create_presets() {
+	let preset_grid = document.getElementById("preset-grid"); 
+	for (let [key, value] of Object.entries(preset_map)) {
+		// make and attach a "preset" div
+		let preset = document.createElement("div");
+		preset.classList.add("preset");
+		preset_grid.appendChild(preset)
+
+		// make and attach a button
+		let button = document.createElement("button");
+		button.classList.add("button");
+		// attach text to the button
+		let text = document.createTextNode(key);
+		button.appendChild(text);
+		// attach an onclick function
+		button.onclick = () => apply_preset(key)
+		preset.appendChild(button)		
+	}
+}
 
 /* payment handling */
 function pay() {
 	console.log("Register click event -- pay button");
 }
+
+// main
+create_presets();
